1c74230ba9a0f92ac63a255322b1b04ee0cf9c10
Dennis Petersen <petersen.dnns@gmail.com>
2024-04-23 17:45:02 +0200

    Update install.sh with Zellij plus alias

--- .zshrc
+++ .zshrc
@@ -22,6 +22,8 @@ alias vim='nvim'
 alias vi='nvim'
 alias v='nvim'
 
+alias zij='zellij'
+
 #
 # History
 #
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,40 +1,75 @@
 #!/bin/bash
 
+#
 # standard packages
+#
+
+## Applications
 
 sudo pacman -S neovim
 sudo pacman -S obsidian
+sudo pacman -S tmux
+sudo pacman -S zellij
+
+## Tools
+
 sudo pacman -S zoxide
 sudo pacman -S fzf
 sudo pacman -S ripgrep
+
+## Fonts
+
 sudo pacman -S ttf-cascadia-code-nerd
 sudo pacman -S ttf-nerd-fonts-symbols
 sudo pacman -S ttf-nerd-fonts-symbols-mono
-sudo pacman -S tmux
 
+#
 # AUR packages
-yay -S goxlr-utility
-yay -S spotify
-yay -S nnn
-yay -S brave-bin
-yay -S zsh
-yay -S bitwarden
-yay -S go-task-bin
+#
 
+## Applications
+
+yay -S brave-bin
+yay -S spotify
+yay -S bitwarden
+
+## Tools
+
+yay -S goxlr-utility
+yay -S nnn
+yay -S go-task-bin
+yay -S zsh
+
+#
+# Plugins
+#
+
+## Z-shell plugin manager
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+
+#
 # Symlinks
+#
+
 ## Create a local symlink named task to go-task-bin executable
 ln -s /usr/bin/go-task ~/.local/bin/task
 
-# git clones
+#
+# Git projects
+#
+
+## Uni-sync (for Lian Li fans)
 mkdir ~/Applications
 git clone https://github.com/EightB1ts/uni-sync.git ~/Applications
 
-# install zsh plugin manager
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+#
+# Miscellaneous
+#
 
+## Version Manager
 curl https://mise.run | sh
 ~/.local/bin/mise --version
 
-# keyboard layout
+## Keyboard layout (English, Danish, 104 keys)
 localectl set-x11-keymap gb,dk pc104 "" grp:win_space_toggle
 setxkbmap gb,dk pc104 "" grp:win_space_toggle
