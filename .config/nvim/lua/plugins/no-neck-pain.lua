7fed693f6e7a8236adc9dc6942f4794e53d05323
Dennis Petersen <petersen.dnns@gmail.com>
2025-04-06 19:14:27 +0200

    Neovim no-neck-pain is based on procentage width, add twilight for code dimming

--- .config/nvim/lua/plugins/no-neck-pain.lua
+++ .config/nvim/lua/plugins/no-neck-pain.lua
@@ -2,8 +2,11 @@ return {
   "shortcuts/no-neck-pain.nvim",
   version = "*",
   config = function()
+    local screen_width = vim.o.columns
+    local width = math.floor(screen_width * 0.6)
+
     require("no-neck-pain").setup({
-      width = 140,
+      width = width,
       autocmds = {
         enableOnVimEnter = true
       },
