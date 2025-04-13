95afe50a9b44a19aaff955ed13e43882cfc0c889
Dennis Petersen <petersen.dnns@gmail.com>
2025-04-13 15:55:25 +0200

    Update Lazy plugins, change dap mappings and zellij keybinds

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -7,15 +7,32 @@
 return {
 	{
 		"mfussenegger/nvim-dap",
-	},
-	{
-		"rcarriga/nvim-dap-ui",
 		dependencies = {
-			"mfussenegger/nvim-dap",
 			"nvim-neotest/nvim-nio",
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+			"leoluz/nvim-dap-go",
+			-- "theHamsta/nvim-dap-virtual-text",
 		},
 		config = function()
-			require("dapui").setup()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			require("dapui").setup({})
+			require("dap-go").setup()
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 		end,
 	},
 	{
